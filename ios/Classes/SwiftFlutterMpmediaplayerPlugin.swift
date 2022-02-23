49a919c67a47be903eb6e9fc1bc35d33966c24b1
nrubin29 <nrubin29@gmail.com>
2022-02-22 20:07:47 -0800

    Fixed album search

--- ios/Classes/SwiftFlutterMpmediaplayerPlugin.swift
+++ ios/Classes/SwiftFlutterMpmediaplayerPlugin.swift
@@ -114,12 +114,11 @@ public class SwiftFlutterMPMediaPlayerPlugin: NSObject, FlutterPlugin {
             }
             
             let query = MPMediaQuery.albums()
-            query.addFilterPredicate(MPMediaPropertyPredicate(value: request.query, forProperty: MPMediaItemPropertyTitle, comparisonType: .contains))
+            query.addFilterPredicate(MPMediaPropertyPredicate(value: request.query, forProperty: MPMediaItemPropertyAlbumTitle, comparisonType: .contains))
             
-            let items = query.items!.getPage(request.limit, request.page).filter { item in
-                item.title != nil && item.artist != nil
-            }.map { item in
-                Album(title: item.title!, artist: item.artist!, artwork: item.artwork?.image(at: CGSize(width: 300, height: 300))?.pngData()?.base64EncodedString())
+            let items = query.collections!.getPage(request.limit, request.page).map { item -> Album in
+                let repItem = item.representativeItem!
+                return Album(title: repItem.albumTitle!, artist: repItem.artist!, artwork: repItem.artwork?.image(at: CGSize(width: 300, height: 300))?.pngData()?.base64EncodedString())
             }
             
             let jsonData = try! SwiftFlutterMPMediaPlayerPlugin.jsonEncoder.encode(items)
